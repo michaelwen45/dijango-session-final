1ef4867f03bb564f0ffa265c287c8e7eb961908a
“MichaelWen <michaelwen.dev@gmail.com>
2016-12-02 14:03:33 -0500

    cambio de conexion mongo

--- apps/taller4/conexionmongo.py
+++ apps/taller4/conexionmongo.py
@@ -5,10 +5,10 @@ from pymongo import ASCENDING, DESCENDING
 
 
 class Connection():
-    client = MongoClient('mongodb://localhost:27017/taller4')
-    bd = client.taller4
-    #client = MongoClient('mongodb://mongouser:pemofra@clusterbigdata57.virtual.uniandes.edu.co/grupo14')
-    #bd = client.grupo14
+    #client = MongoClient('mongodb://localhost:27017/taller4')
+    #bd = client.taller4
+    client = MongoClient('mongodb://mongouser:pemofra@clusterbigdata57.virtual.uniandes.edu.co/grupo14')
+    bd = client.grupo14
 
 
     def set_client(self, host, port):
